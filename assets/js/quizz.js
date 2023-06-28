6450071b57e2ee025dc22be977198bff6d6b7c9c
Frangilio <farasy.frangilio@gmail.com>
2023-06-28 16:24:00 +0300

    Amelioration du quizz dans l'enigme

---
+++ assets/js/quizz.js
@@ -0,0 +1,40 @@
+let reponse,
+    numQuizz = 0;
+
+
+$('#envoyer').click(function () {
+    $.ajax({
+        url: './assets/json/quizz_enigme.json',
+        datatype: 'json',
+        success: (quizz) => {
+            let reponse = $('#input').val();
+            
+            if (reponse) {
+                if (reponse === quizz[numQuizz].reponse) {
+                    $('#alphabet').slideUp(1000).slideDown(1000);
+                    setTimeout(() => {
+                        $('#enonce').empty();
+                        $('#enonce').text(quizz[numQuizz+1].enonce);
+                        $('#textQuizz').text(quizz[numQuizz+1].enigme);
+                        $('.enigme-wakanda').remove();
+                        $('#input').empty();
+                        $('.p1').empty();
+                        $('.p1').text('Le savais tu ?');
+                        $('.paragraphe-forever').empty();
+                        $('.p2').text(quizz[numQuizz+1].saisTu);
+                        numQuizz++;
+                    }, 250);
+                    
+                }else {
+                    console.log(reponse);
+                    console.log('Oops! Recommencer');
+                }
+            }else {
+                console.log('Choisit une reponses')
+            }
+        },
+        error: function (quizz) {
+            console.log(quizz);
+        }
+    });
+});
